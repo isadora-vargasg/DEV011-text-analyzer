e10ddf1a1a408acd5716bcb5dadc30dad0f3b181
isadora-vargasg <isadora.vargasg@gmail.com>
2023-08-29 16:05:52 -0600

    Funciones de analizer ya trabajan correctamente

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,20 +1,47 @@
 const analyzer = {  
   getWordCount: (text) => {
+    const palabras = text.split(" ");
+    const palabrasNoEspacio = palabras.filter(function(palabra){
+      return palabra !== '';
+    });
+    return palabrasNoEspacio.length;
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCount: (text) => {
+    return text.length;
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCountExcludingSpaces: (text) => {
+    const textNoEspacios = text.replace(/[^\w\d]/g, ''); //expresión regular para reemplazar cualquier caracter que no sea una letra o número (\w representa letras y números, y \d representa solo números) con una cadena vacía
+    const contCaractNoEspacios = textNoEspacios.length;
+    return contCaractNoEspacios;
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {
+    const palabrasTotales = text.split(/\s+/);
+    const caracteresTotales = palabrasTotales.join('').length;
+    if (palabrasTotales.length === 0){
+      return 0;
+    }
+    const promedio = caracteresTotales / palabrasTotales.length;
+    return promedio;
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberCount: (text) => {
+    const numerosEncontrados = text.match(/-?\d+(\.\d+)?/g);
+    const contNumeros = numerosEncontrados ? numerosEncontrados.length : 0;
+    return contNumeros;
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberSum: (text) => {
+    const numerosEncontrados = text.match(/-?\d+(\.\d+)?/g); //buscar todos los números en el texto, incluyendo números enteros y decimales
+    let sumatoria = 0;
+    if (numerosEncontrados) {
+      for (let i = 0; i < numerosEncontrados.length; i++) {
+        sumatoria += parseFloat(numerosEncontrados[i]);
+      }
+    }
+    return sumatoria
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import analyzer from './analyzer.js';
 
+const textarea = document.querySelector("textarea");
+
+
+textarea.addEventListener("keyup", (event) => {
+  console.log(analyzer.getNumberSum(event.target.value))
+  // analyzer.getAverageWordLength()
+});
+
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
